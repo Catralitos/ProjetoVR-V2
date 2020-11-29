15bc08c7594e25a701ff223b9c0d4a08a612dc21
Miguel Malheiro <32638883+Maldracus@users.noreply.github.com>
2020-11-29 14:46:46 +0000

    rotation v2

--- Assets/MyRotation.cs
+++ Assets/MyRotation.cs
@@ -5,23 +5,26 @@ using UnityEngine;
 public class MyRotation : MonoBehaviour
 {
     public GameObject virtual_World;
-    public Transform rot_reference;
+    public Quaternion rot_reference;
     public float rotation_gain;
     private float old_rot;
 
     private void OnTriggerEnter(Collider other)
     {
+        
         if (other.CompareTag("Player"))
         {
+            rot_reference = other.GetComponent<RotReference>().camRig.centerEyeAnchor.rotation;
             print("started");
-            old_rot = other.transform.rotation.eulerAngles.y;
+            old_rot = rot_reference.eulerAngles.y;
         }
     }
     private void OnTriggerStay(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            float new_rot = other.transform.rotation.eulerAngles.y;
+            rot_reference = other.GetComponent<RotReference>().camRig.centerEyeAnchor.rotation;
+            float new_rot = rot_reference.eulerAngles.y;
             //if (System.Math.Abs(new_rot + 360 - old_rot) > 300) new_rot = (360 - System.Math.Abs(new_rot))*(new_rot/ System.Math.Abs(new_rot));
             if (System.Math.Abs( new_rot-old_rot)>0.01)
             {
@@ -36,7 +39,7 @@ public class MyRotation : MonoBehaviour
                     print("dif "+ diff);
                     print("world "+world_rot);
                 }
-                virtual_World.transform.RotateAround(other.transform.position, Vector3.up, world_rot);
+                virtual_World.transform.RotateAround(other.GetComponent<TPreference>().player.transform.position, Vector3.up, world_rot);
             }
             old_rot = new_rot;
         }
