c39a879bcce0947ce9053f730c58904fa2555642
Bernardo Pinto <bernardo.ra.pinto@gmail.com>
2020-11-30 15:08:18 +0000

    prefabs : WIP

--- Assets/Our_Stuff/Scripts/RoomDirections.cs
+++ Assets/Our_Stuff/Scripts/RoomDirections.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //Varias direções para ligar as salas (supostamente faltam mais)
-public enum RoomDir { Root, North, South, East, West };
+public enum RoomDir { Root, 
+    North_L, North_R, North_LR,
+    South_L,South_R, South_LR,
+    East_L, East_R, East_LR,
+    West_L, West_R,West_LR};
 
 //Este script é para dar attach aos prefabs das salas e preencher manualmente
 public class RoomDirections : MonoBehaviour
