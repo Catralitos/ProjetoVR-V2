bde3f80c84731de7523d4b0626535a9054b2cce7
Bernardo Pinto <bernardo.ra.pinto@gmail.com>
2020-11-30 16:16:22 +0000

    2 Way prefabs.  RoomDir updated

--- Assets/Our_Stuff/Scripts/RoomDirections.cs
+++ Assets/Our_Stuff/Scripts/RoomDirections.cs
@@ -9,7 +9,8 @@ public enum RoomDir
     North_L = 1, North_R = 2, North_LR = 3,
     South_L = 4, South_R = 5, South_LR = 6,
     East_L = 7, East_R = 8, East_LR = 9,
-    West_L = 10, West_R = 11, West_LR = 12
+    West_L = 10, West_R = 11, West_LR = 12,
+    South_RL = 13, East_RL = 14, North_RL = 15, West_RL = 16,
 };
 
 //Este script é para dar attach aos prefabs das salas e preencher manualmente
