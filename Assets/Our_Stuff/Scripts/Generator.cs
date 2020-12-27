94b268071bd83617546d2e15b23a3a5808ca2fd8
Hélio <56256043+Helio-Martins@users.noreply.github.com>
2020-12-27 16:24:13 +0000

    Instancing Corridor Entrances
    Geração do início dos corredores, nomeadamente as "mini walls" e "outer walls"

--- Assets/Our_Stuff/Scripts/Generator.cs
+++ Assets/Our_Stuff/Scripts/Generator.cs
@@ -4,35 +4,50 @@ using UnityEngine;
 
 public class Generator : MonoBehaviour
 {
-    public static GameObject roof;
-    public static GameObject outerFloor;
-    public static GameObject outerWalls;
-    public static GameObject miniWallLeft;
-    public static GameObject miniWallRight;
-    public static GameObject roomTpLeft;
-    public static GameObject roomTpRight;
-    public static GameObject corridorTpLeft;
-    public static GameObject corridorTpRight;
-    public static List<Material> innerMaterials;
+    public static Generator generator;
+
+    private void Awake()
+    {
+        generator = this;
+    }
+
+    public GameObject roof;
+    public GameObject outerFloor;
+    public GameObject outerWalls;
+    public GameObject miniWallLeft;
+    public GameObject miniWallRight;
+    public GameObject roomTpLeft;
+    public GameObject roomTpRight;
+    public GameObject corridorTpLeft;
+    public GameObject corridorTpRight;
+    public List<RoomProperties> innerMaterials;
 
     [System.Serializable]
     public struct RoomProperties
     {
         [SerializeField]
-        GameObject innerFloor;
+        public GameObject innerFloor;
         [SerializeField]
-        GameObject innerWalls;
+        public GameObject innerWalls;
         [SerializeField]
-        GameObject innerDoorframe;
+        public GameObject innerDoorframe;
         [SerializeField]
-        bool hasSpecialEffects;
+        public bool hasSpecialEffects;
         [SerializeField]
-        GameObject specialEffects;
+        public GameObject specialEffects;
     }
 
     public class Node
     {
+        public GameObject node;
         public string id;
+
+        protected void CreateOuterShell()
+        {
+            GameObject.Instantiate(generator.roof, node.transform);
+            GameObject.Instantiate(generator.outerFloor, node.transform);
+            GameObject.Instantiate(generator.outerWalls, node.transform);
+        }
     }
 
     public class Room : Node
@@ -47,7 +62,7 @@ public class Generator : MonoBehaviour
             {
                 id += (int)Random.Range(0, 1);
             }
-            properties = Random.Range(0, innerMaterials.Count - 1);
+            properties = Random.Range(0, generator.innerMaterials.Count - 1);
             GenerateCorridors(null, -1);
         }
 
@@ -87,49 +102,101 @@ public class Generator : MonoBehaviour
         public Corridor(Room room, int entrance)
         {
             origin = room;
+            node = new GameObject("");
             int way = 1;
             if (entrance % 2 == 0)
                 way = -1;
             id += entrance;
             entrance -= way;
+
+            CreateOuterShell();
+            CreateEntrance(true, entrance, way);
+
             int corridorsRemaining = (int)Random.Range(0, 3);
-            //TODO FUNÇAO PRIVADA DAQUI PARA BAIXO
-            int exit;
-            if (corridorsRemaining == 0)
+            GenerateDestination(corridorsRemaining, entrance, way);
+        }
+
+        private void CreateEntrance(bool originIsRoom, int entrance, int way)
+        {
+            if (originIsRoom)
             {
-                exit = entrance + 2 *way;
-                id += exit;
-                string roomId = "";
-                for (int i = 0; i < 8; i++)
+                string miniWalls = "";
+                for (int i = 1; i < 4; i++)
                 {
-                    if (i == exit)
-                        roomId += 1;
-                    else //if (depth != depth max)
-                        roomId += (int)Random.Range(0, 1);
-                    //else
-                        //roomId += 0
+                    int position = entrance - i * way;
+                    position = Wrap(position);
+                    if (origin.id[position] == 0)
+                    {
+                        miniWalls += position;
+                        if (position % 2 == 0)
+                        {
+                            Rotate(GameObject.Instantiate(generator.miniWallLeft, node.transform), position);
+                        } 
+                        else
+                        {
+                            Rotate(GameObject.Instantiate(generator.miniWallRight, node.transform), position);
+                        }
+                    } else
+                    {
+                        miniWalls += -1;
+                    }
                 }
-                int roomProperties = Random.Range(0, innerMaterials.Count - 1);
-                destination = new Room(roomId, roomProperties, this, exit);
+                if (miniWalls[1] != -1 && miniWalls[2] != -1)
+                {
+                    Rotate(GameObject.Instantiate(generator.innerMaterials[((Room)origin).properties].innerWalls, node.transform), Wrap(entrance - way * 2));
+                } 
+                else
+                {
+                    Rotate(GameObject.Instantiate(generator.innerMaterials[((Room)origin).properties].innerDoorframe, node.transform), Wrap(entrance - way * 2));
+                }
             }
-            else
+        }
+
+        private void Rotate(GameObject obj, int position)
+        {
+            switch (position)
             {
-                exit = entrance + 3 * way;
-                id += exit;
-                destination = new Corridor(this, exit, corridorsRemaining - 1);
+                case 0:
+                case 1:
+                    break;
+                case 2:
+                case 3:
+                    obj.transform.Rotate(new Vector3(0, 90, 0));
+                    break;
+                case 4:
+                case 5:
+                    obj.transform.Rotate(new Vector3(0, 180, 0));
+                    break;
+                case 6:
+                case 7:
+                    obj.transform.Rotate(new Vector3(0, 270, 0));
+                    break;
+                default:
+                    obj.transform.Rotate(new Vector3(0, 45, 0));
+                    break;
             }
+        }
+
+        private void CreateExit()
+        {
+
         }
 
         public Corridor(Corridor corridor, int entrance, int corridorsRemaining)
         {
             origin = corridor;
+
             int way = 1;
             if (entrance % 2 == 0)
                 way = -1;
             entrance -= way;
             id += entrance;
             corridorsRemaining--;
-            //TODO FUNÇAO PRIVADA DAQUI PARA BAIXO
+            GenerateDestination(corridorsRemaining, entrance, way);
+        }
+
+        private void GenerateDestination(int corridorsRemaining, int entrance, int way)
+        {
             int exit;
             if (corridorsRemaining == 0)
             {
@@ -145,7 +212,8 @@ public class Generator : MonoBehaviour
                     //else
                     //roomId += 0
                 }
-                int roomProperties = Random.Range(0, innerMaterials.Count - 1);
+                int roomProperties = Random.Range(0, generator.innerMaterials.Count - 1);
+
                 destination = new Room(roomId, roomProperties, this, exit);
             }
             else
@@ -154,6 +222,19 @@ public class Generator : MonoBehaviour
                 id += exit;
                 destination = new Corridor(this, exit, corridorsRemaining - 1);
             }
+        }
+
+        private int Wrap(int position)
+        {
+            if (position< 0)
+            {
+                position += 8;
+            }
+            if (position > 7)
+            {
+                position -= 8;
+            }
+            return position;
         }
 
     }
